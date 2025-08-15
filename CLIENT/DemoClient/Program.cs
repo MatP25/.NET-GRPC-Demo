87c77835fe8043a3e65be7d641deabc9a791fb10
MatP25 <matiaspiriz96@gmail.com>
2025-08-15 15:16:57 -0300

    tutorial / working api and client

---
+++ CLIENT/DemoClient/Program.cs
@@ -0,0 +1,10 @@
+﻿using Grpc.Net.Client;
+using DemoClient;
+
+using var channel = GrpcChannel.ForAddress("https://localhost:5279");
+var client = new Greeter.GreeterClient(channel);
+var reply = await client.SayHelloAsync(new HelloRequest { Name = "Pepe" });
+Console.WriteLine("Greeting: " + reply.Message);
+Console.WriteLine("Press any key to exit...");
+Console.ReadKey();
+
